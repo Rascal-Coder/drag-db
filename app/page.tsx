078f15dfc393c5f6994de00ea22bb63670299ad2
rascal <ras-cal@gmail.com>
2025-10-28 16:11:17 +0800

    chore: link to useRouter

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
+"use client";
 import { LinkIcon } from "lucide-react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-
 export default function Home() {
+  const router = useRouter();
   return (
-    <Link href="/editor">
-      <Button>
-        to editor
-        <LinkIcon />
-      </Button>
-    </Link>
+    <Button onClick={() => router.push("/editor")}>
+      to editor
+      <LinkIcon />
+    </Button>
   );
 }
